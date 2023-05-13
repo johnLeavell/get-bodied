ef88173aa66c09c74d4e92d0119ee12080ac5f2a
John Leavell <johnleavell@gmail.com>
2023-05-13 10:13:50 -0500

    updated crate on messages

--- app/controllers/ai_messages_controller.rb
+++ app/controllers/ai_messages_controller.rb
@@ -1,4 +1,6 @@
 class AiMessagesController < ApplicationController
+  require "openai"
+
   def index
 
     @q = AiMessage.all.order({ :created_at => :desc }).ransack(params[:q])
@@ -7,31 +9,32 @@ class AiMessagesController < ApplicationController
     render({ :template => "ai_messages/index.html.erb" })
   end
 
-  def show
-    the_id = params.fetch("path_id")
+  # def show
+  #   the_id = params.fetch("path_id")
 
-    matching_ai_messages = AiMessage.where({ :id => the_id })
+  #   matching_ai_messages = AiMessage.where({ :id => the_id })
 
-    @the_ai_message = matching_ai_messages.at(0)
+  #   @the_ai_message = matching_ai_messages.at(0)
 
-    render({ :template => "ai_messages/show.html.erb" })
-  end
+  #   render({ :template => "ai_messages/show.html.erb" })
+  # end
 
   def create
-    the_ai_message = AiMessage.new
-    the_ai_message.role = params.fetch("query_role")
-    the_ai_message.content = params.fetch("query_content")
-    the_ai_message.user = @current_user
-
-    if the_ai_message.valid?
-      the_ai_message.save
-
-      require "openai"
+    the_ai_message = AiMessage.new(
+      role: params.fetch("query_role"),
+      content: params.fetch("query_content"),
+      user: @current_user
+    )
       
-      client = OpenAI::Client.new(access_token: ENV.fetch("CHAT_API"),
-      request_timeout: 240)
-
-
+      if the_ai_message.save
+        client = OpenAI::Client.new(access_token: ENV.fetch("CHAT_API"), request_timeout: 240)
+      
+        the_workout = Workout.find_by(id: the_ai_message.user.id)
+        api_messages_array = AiMessage.where(id: the_workout).map do |the_message|
+          { role: the_message.role, content: the_message.content }
+        end
+      end
+      
 
       response = client.chat(
         parameters: {
@@ -48,47 +51,44 @@ class AiMessagesController < ApplicationController
           temperature: 0.7,
         },
       )
-      # the_ai_user_messages = the_ai_message.map { |m| {role: m.role, content: m.content}}
-      # user_messages = User.find_by_id(@current_user)
-      #<AiMessagesController>)> user = User.find_by(@current_user_id)
-        #user_messages = user.ai_messages.map { |m| { role: m.role, content: m.content}}
 
       the_ai_message = AiMessage.new
       message = response.dig("choices", 0, "message")
       the_ai_message.role = message.dig("role")
       the_ai_message.content = message.dig("content")
       the_ai_message.user = @current_user
+      
+    if 
       the_ai_message.save
-
-
-      redirect_to("/ai_messages", { :notice => "Ai message created successfully." })
+      redirect_to("/ai_messages", { :notice => "Workout created successfully." })
     else
       redirect_to("/ai_messages", { :alert => the_ai_message.errors.full_messages.to_sentence })
     end
   end
 
-  def update
-    the_id = params.fetch("path_id")
-    the_ai_message = AiMessage.where({ :id => the_id }).at(0)
+  # def update
+  #   the_id = params.fetch("path_id")
+  #   the_ai_message = AiMessage.where({ :id => the_id }).at(0)
 
-    the_ai_message.role = params.fetch("query_role")
-    the_ai_message.content = params.fetch("query_content")
-    the_ai_message.user_id = params.fetch("query_user_id")
+  #   the_ai_message.role = params.fetch("query_role")
+  #   the_ai_message.content = params.fetch("query_content")
+  #   the_ai_message.user_id = params.fetch("query_user_id")
 
-    if the_ai_message.valid?
-      the_ai_message.save
-      redirect_to("/ai_messages/#{the_ai_message.id}", { :notice => "Ai message updated successfully." })
-    else
-      redirect_to("/ai_messages/#{the_ai_message.id}", { :alert => the_ai_message.errors.full_messages.to_sentence })
-    end
-  end
+  #   if the_ai_message.valid?
+  #     the_ai_message.save
+  #     redirect_to("/ai_messages/#{the_ai_message.id}", { :notice => "Ai message updated successfully." })
+  #   else
+  #     redirect_to("/ai_messages/#{the_ai_message.id}", { :alert => the_ai_message.errors.full_messages.to_sentence })
+  #   end
+  # end
 
-  def destroy
-    the_id = params.fetch("path_id")
-    the_ai_message = AiMessage.where({ :id => the_id }).at(0)
+  # def destroy
+  #   the_id = params.fetch("path_id")
+  #   the_ai_message = AiMessage.where({ :id => the_id }).at(0)
 
-    the_ai_message.destroy
+  #   the_ai_message.destroy
 
-    redirect_to("/ai_messages", { :notice => "Ai message deleted successfully." })
-  end
+  #   redirect_to("/ai_messages", { :notice => "Ai message deleted successfully." })
+  # end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
     matching_user = User.where({ :id => path_user })
     @the_user = matching_user.first
 
+    # message = response.dig("choices", 0, "message")
+    # the_ai_message.role = message.dig("role")
+    # the_ai_message.content = message.dig("content")
+    # the_ai_message.user = @current_user
+
     @user_workouts = AiMessage.where({ :id => @the_user.id}).first
 # binding.pry
     render template: "users/index.html.erb"
